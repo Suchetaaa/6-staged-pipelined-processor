4b3ec99bdc177ba326220eb36f2ab9db478c4a86
Suchetaaa <sucheta317@gmail.com>
2018-12-02 19:12:55 +0530

    changes in wb

--- writeback.vhd
+++ writeback.vhd
@@ -66,70 +66,66 @@ end entity ; -- instruction_fetch
 architecture arch of write_back is
 
 begin
-	final_write_signal : process(clk)
+	final_write_signal : process(clk, rf_carry_reg_out, rf_zero_reg_out, alu1_carry_wb, alu1_zero_wb, right_shift_lm_sm_bit_wb, data_extender_out_wb, pc_out_wb, alu1_out_wb, mem_data_out)
 	begin
-		if (clk'event and clk = '1') then 
-			--ADC  
-			if opcode_wb = "0000" and cz_wb = "10" then 
-				rf_write_final <=  rf_carry_reg_out;
-				carry_en_final <= rf_carry_reg_out;
-				zero_en_final <= rf_carry_reg_out;
-				carry_val_final <= alu1_carry_wb;
-				zero_val_final <= alu1_zero_wb;
-			--ADZ
-			elsif opcode_wb = "0000" and cz_wb = "01" then 
-				rf_write_final <= rf_zero_reg_out;
-				carry_en_final <= rf_zero_reg_out;
-				zero_en_final <= rf_zero_reg_out;
-				carry_val_final <= alu1_carry_wb;
-				zero_val_final <= alu1_zero_wb;
-			--NDC
-			elsif opcode_wb = "0010" and cz_wb = "10" then
-				rf_write_final <= rf_carry_reg_out;
-				zero_en_final <= rf_carry_reg_out;
-				carry_en_final <= '0';
-				zero_val_final <= alu1_zero_wb;
-				carry_val_final <= '0';
-			--NDZ
-			elsif opcode_wb = "0010" and cz_wb = "01" then
-				rf_write_final <= rf_zero_reg_out;
-				zero_en_final <= rf_zero_reg_out;
-				carry_en_final <= '0';
-				zero_val_final <= alu1_zero_wb;
-				carry_val_final <= '0';
-			--LM SM done 
-			elsif (opcode_wb = "0110" or opcode_wb = "0111") then 
-				rf_write_final <= right_shift_lm_sm_bit_wb;
-				zero_en_final <= '0';
-				carry_en_final <= '0';
-				carry_val_final <= '0';
-				zero_val_final <= '0';
-			elsif (opcode_wb = "0100" and mem_data_out = "0000000000000000") then
-				rf_write_final <= '1';
-				carry_en_final <= '0';
-				zero_en_final <= '1';
-				carry_val_final <= '0';
-				zero_val_final <= '1';
-			else 
-				rf_write_final <= rf_write_wb;
-				carry_en_final <= carry_en_wb;
-				zero_en_final <= zero_en_mem_wb or zero_en_alu_wb;
-				zero_val_final <= alu1_zero_wb;
-				carry_val_final <= alu1_carry_wb;
-			end if;
+		--ADC  
+		if opcode_wb = "0000" and cz_wb = "10" then 
+			rf_write_final <=  rf_carry_reg_out;
+			carry_en_final <= rf_carry_reg_out;
+			zero_en_final <= rf_carry_reg_out;
+			carry_val_final <= alu1_carry_wb;
+			zero_val_final <= alu1_zero_wb;
+		--ADZ
+		elsif opcode_wb = "0000" and cz_wb = "01" then 
+			rf_write_final <= rf_zero_reg_out;
+			carry_en_final <= rf_zero_reg_out;
+			zero_en_final <= rf_zero_reg_out;
+			carry_val_final <= alu1_carry_wb;
+			zero_val_final <= alu1_zero_wb;
+		--NDC
+		elsif opcode_wb = "0010" and cz_wb = "10" then
+			rf_write_final <= rf_carry_reg_out;
+			zero_en_final <= rf_carry_reg_out;
+			carry_en_final <= '0';
+			zero_val_final <= alu1_zero_wb;
+			carry_val_final <= '0';
+		--NDZ
+		elsif opcode_wb = "0010" and cz_wb = "01" then
+			rf_write_final <= rf_zero_reg_out;
+			zero_en_final <= rf_zero_reg_out;
+			carry_en_final <= '0';
+			zero_val_final <= alu1_zero_wb;
+			carry_val_final <= '0';
+		--LM SM done 
+		elsif (opcode_wb = "0110" or opcode_wb = "0111") then 
+			rf_write_final <= right_shift_lm_sm_bit_wb;
+			zero_en_final <= '0';
+			carry_en_final <= '0';
+			carry_val_final <= '0';
+			zero_val_final <= '0';
+		elsif (opcode_wb = "0100" and mem_data_out = "0000000000000000") then
+			rf_write_final <= '1';
+			carry_en_final <= '0';
+			zero_en_final <= '1';
+			carry_val_final <= '0';
+			zero_val_final <= '1';
+		else 
+			rf_write_final <= rf_write_wb;
+			carry_en_final <= carry_en_wb;
+			zero_en_final <= zero_en_mem_wb or zero_en_alu_wb;
+			zero_val_final <= alu1_zero_wb;
+			carry_val_final <= alu1_carry_wb;
 		end if;
-
-		if (clk'event and clk = '1') then 
-			if rf_data_select_wb = "000" then 
-				rf_data_final <= alu1_out_wb;
-			elsif rf_data_select_wb = "001" then
-				rf_data_final <= pc_out_wb;
-			elsif rf_data_select_wb = "010" then
-				rf_data_final <= data_extender_out_wb;
-			elsif rf_data_select_wb = "011" then
-				rf_data_final <= mem_data_out;
-			end if;
-		end if; 
+		
+		if rf_data_select_wb = "000" then 
+			rf_data_final <= alu1_out_wb;
+		elsif rf_data_select_wb = "001" then
+			rf_data_final <= pc_out_wb;
+		elsif rf_data_select_wb = "010" then
+			rf_data_final <= data_extender_out_wb;
+		elsif rf_data_select_wb = "011" then
+			rf_data_final <= mem_data_out;
+		end if;
 
 		rf_a3_final <= rf_a3_wb; 
 
